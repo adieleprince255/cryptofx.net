515e2942f513014b4662b4b05f3dc82e5145c85e
adieleprince255 <123737341+adieleprince255@users.noreply.github.com>
2023-11-24 11:47:57 +0100

    Add files via upload

---
+++ cryptoxtreme.net/m.cryptoxtreme.net/assets/templates/neo_dark/css/color-9986.php
@@ -0,0 +1,57 @@
+
+
+
+
+body, .modal-content-bg, .user-sidebar.style--xl, .form-control,.header-section.header-fixed {
+	background-color: #110d2f !important;
+}
+
+.footer-section::before, .cmn-accordion.style--two .card-header .acc-btn::after,.base--bg,*::-webkit-scrollbar-button,*::-webkit-scrollbar-thumb,::selection   {
+    background-color: #fdf7ff !important;
+}
+
+.footer-widget .social-links li a:hover , .social-links li a:hover , .testimonial-single i, .pricing-item__header .package__price , .btn-primary, .overview-item span, .feature-thumb.icon, 
+.feature--item.active .feature-content .title, .feature--item:hover .feature-content .title, .nav-tabs li a.color-one, .nav-tabs li a.color-one , .nav-tabs li a.color-two , .single-post__footer .share a:hover, .captcha span, .contact-item .icon ,.inner-hero-content .page__breadcums a,.privacy-links li a:hover, .base--color,.work-item .work-icon i,.work-item .work-content .sub-title {
+	color: #fdf7ff !important;
+}
+
+.btn-primary:hover {
+    color: #fff;
+}
+.footer-bottom{
+    border-top: 1px solid #fdf7ff63;
+}
+.header-top {
+    padding-top: 10px;
+    padding-bottom: 15px;
+    border-bottom: 1px solid #fdf7ff50;
+}
+.work-item .work-icon::before {
+    border: 2px dashed #fdf7ff;
+}
+@media (max-width: 1199px){
+
+    .navbar-toggler {
+        margin-right: 0px;
+    }
+    .header-section {
+        background-color: #110d2f;
+    }
+}
+
+.inner-hero-section{
+    border-bottom: 5px solid #fdf7ff;
+}
+
+.list-group-item, .referral-form .input-group-text {
+    border: 1px solid #fdf7ff20;
+}
+.hero-section {
+    border-bottom: 5px solid #fdf7ff;
+}
+.cmn-btn, .referral-form .input-group-text {
+    background: #fdf7ff;
+}
+
+.cookie__wrapper{
+    background: #110d2f}
